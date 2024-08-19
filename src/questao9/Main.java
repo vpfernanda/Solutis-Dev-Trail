8d765ab5cc9146acad711492b6063e6eed04c744
vpfernanda <fernandavpagano@gmail.com>
2024-08-19 10:39:27 -0300

    "Solucao da questao 9."

---
+++ src/questao9/Main.java
@@ -0,0 +1,15 @@
+package questao9;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        final double PI = 3.14;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o raio da circunferência: ");
+        double raio = scanner.nextDouble();
+        double area = (raio * raio) * PI;
+        area = Math.ceil(area); //valor arredondado.
+        System.out.println("Área: "+area);
+    }
+}
