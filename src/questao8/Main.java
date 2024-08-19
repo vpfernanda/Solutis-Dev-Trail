2462c936e11f0413731b1b34fe09fbcbb87c0813
vpfernanda <fernandavpagano@gmail.com>
2024-08-19 08:10:33 -0300

    Resolucao Questao 8 - Main.java

---
+++ src/questao8/Main.java
@@ -0,0 +1,15 @@
+package questao8;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite uma palavra: ");
+        String palavra = scanner.next();
+        Palindromo palavraParaVerif = new Palindromo(palavra);
+        System.out.println(palavraParaVerif.conferePalindromo());
+    }
+
+
+}
